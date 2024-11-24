9fbf84f3bb898f8410cb2a46a44856cbd9adb46c
nguyenphuctinh <nguyenphuctinh>
2024-11-24 11:49:45 +0700

    Login

--- frontend/nichoshop.ui/src/main.js
+++ frontend/nichoshop.ui/src/main.js
@@ -1,8 +1,21 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router';
+import { createApp } from "vue";
+import { createVfm } from "vue-final-modal";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
+import "./style.css";
+import "vue-final-modal/style.css";
+import "primeicons/primeicons.css";
+
+import App from "./App.vue";
+import router from "./router";
 
 const app = createApp(App);
+const vfm = createVfm();
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
+app.use(vfm);
 app.use(router);
-app.mount('#app');
+app.mount("#app");
--- frontend/nichoshop.ui/src/views/LoginPage.vue
+++ frontend/nichoshop.ui/src/views/LoginPage.vue
@@ -1,27 +1,84 @@
 <template>
-  <div style="text-align: center; margin-top: 50px">
-    <h2>Đăng nhập Tĩnh làm nhé</h2>
-    <button @click="handleLogin">Login</button>
+  <div>
+    <div class="logo"></div>
+    <Form
+      v-slot="$form"
+      :resolver="resolver"
+      :initialValues="initialValues"
+      @submit="onFormSubmit"
+      class="flex flex-col gap-4 w-full sm:w-64"
+    >
+      <div class="flex flex-col gap-1">
+        <InputGroup>
+          <InputGroupAddon>
+            <i class="pi pi-user"></i>
+          </InputGroupAddon>
+          <InputText v-model="text1" placeholder="Username" />
+        </InputGroup>
+        <Password
+          name="password"
+          placeholder="Password"
+          :feedback="false"
+          fluid
+        />
+        <template v-if="$form.password?.invalid">
+          <Message
+            v-for="(error, index) of $form.password.errors"
+            :key="index"
+            severity="error"
+            size="small"
+            variant="simple"
+            >{{ error.message }}</Message
+          >
+        </template>
+      </div>
+      <Button type="submit" severity="secondary" label="Submit" />
+    </Form>
   </div>
 </template>
-
-<script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { VueFinalModal } from "vue-final-modal";
+import { zodResolver } from "@primevue/forms/resolvers/zod";
+// import { useToast } from "primevue/usetoast";
+import { z } from "zod";
 import { useRouter } from "vue-router";
-
-export default {
-  setup() {
-    const router = useRouter();
-
-    const handleLogin = () => {
-      // Điều hướng đến trang chủ khi nhấn nút
-      router.push("/home");
-    };
-
-    return { handleLogin };
-  },
+// const toast = useToast();
+const initialValues = ref({
+  password: "",
+});
+const router = useRouter();
+const resolver = ref(
+  zodResolver(
+    z.object({
+      password: z
+        .string()
+        .min(3, { message: "Minimum 3 characters." })
+        .max(8, { message: "Maximum 8 characters." })
+        .refine((value) => /[a-z]/.test(value), {
+          message: "Must have a lowercase letter.",
+        })
+        .refine((value) => /[A-Z]/.test(value), {
+          message: "Must have an uppercase letter.",
+        }),
+      // .refine((value) => /d/.test(value), {
+      //   message: "Must have a number.",
+      // }),
+    })
+  )
+);
+const onFormSubmit = ({ valid }) => {
+  if (valid) {
+    router.push("/home");
+  }
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.logo {
+  height: 40px;
+  width: 213px;
+  background-image: url("@/assets/images/NS Logo.svg");
+}
 /* Thêm CSS tùy chỉnh nếu cần */
 </style>
