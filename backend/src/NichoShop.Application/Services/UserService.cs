2154457944a064488efeca5cf3b833fee030f8cc
thangnv-0703 <sunshines0703@gmail.com>
2024-11-28 21:21:44 +0700

    Featuers/nic 42 create data model for backend and config database (#4)
    * update appsetting.json
    
    * Update user flow
    
    * update app setting
    
    * add signup flow

--- backend/src/NichoShop.Application/Services/UserService.cs
+++ backend/src/NichoShop.Application/Services/UserService.cs
@@ -2,22 +2,36 @@
 using NichoShop.Application.Interfaces;
 using NichoShop.Application.Models.Dtos.Request.User;
 using NichoShop.Application.Models.Dtos.Response.User;
+using NichoShop.Domain.AggergateModels.UserAggregate;
+using NichoShop.Domain.Repositories;
 
 namespace NichoShop.Application.Services;
 
 public class UserService : IUserService
 {
     private readonly IValidator<CreateUserRequestDto> _createUserValidator;
+    private readonly IUserRepository _userRepository;
 
-    public UserService(IValidator<CreateUserRequestDto> createUserValidator)
+    public UserService(IValidator<CreateUserRequestDto> createUserValidator, IUserRepository userRepository)
     {
         _createUserValidator = createUserValidator;
+        _userRepository = userRepository;
     }
 
-    public Task<Guid> CreateUserAsync(CreateUserRequestDto requestDto)
+    public async Task<Guid> CreateUserAsync(CreateUserRequestDto requestDto)
     {
         _createUserValidator.ValidateAndThrow(requestDto);
-        throw new NotImplementedException();
+
+        var userWithPhoneAndEmail = await _userRepository.FindUserByPhoneNumberOrEmail(requestDto.PhoneNumber, requestDto.Email);
+
+        if (userWithPhoneAndEmail is not null)
+        {
+            throw new Exception("User with this phone number or email already exists");
+        }
+        var user = new User(requestDto.FullName, requestDto.Email, requestDto.PhoneNumber, requestDto.Password, requestDto.UserName);
+        _userRepository.Add(user);
+        await _userRepository.SaveChangesAsync();
+        return user.Id;
     }
 
     public Task<LoginResponseDto> LoginAsync(LoginRequestDto requestDto)
--- backend/src/NichoShop.Domain/Repositories/IUserRepository.cs
+++ backend/src/NichoShop.Domain/Repositories/IUserRepository.cs
@@ -1,7 +1,8 @@
 ﻿using NichoShop.Domain.AggergateModels.UserAggregate;
+using NichoShop.Domain.Seedwork;
 
 namespace NichoShop.Domain.Repositories;
-public interface IUserRepository 
+public interface IUserRepository : IRepository<User>
 {
-    Task<User> FindUserByPhoneNumberOrEmail(string phoneNumber, string email);
+    Task<User?> FindUserByPhoneNumberOrEmail(string phoneNumber, string email);
 }
