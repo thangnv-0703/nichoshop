157ab59e71d92d4d922ab0d14116ed8d897e69cf
littlelovevu <69585816+littlelovevu@users.noreply.github.com>
2024-12-15 23:02:04 +0700

    Commit API Location (#26)

---
+++ backend/src/NichoShop.Application/Services/LocationService.cs
@@ -0,0 +1,15 @@
+﻿using NichoShop.Application.Interfaces;
+using NichoShop.Application.Models.ViewModels;
+using NichoShop.Application.Queries;
+
+namespace NichoShop.Application.Services;
+
+public class LocationService(IQueryService queryService) : ILocationService
+{
+    private readonly IQueryService _queryService = queryService;
+
+    public async Task<List<LocationViewModel>> GetLocationAsync(int type, string parentCode)
+    {
+        return await _queryService.GetLocationViewModelsAsync(type, parentCode);
+    }
+}
