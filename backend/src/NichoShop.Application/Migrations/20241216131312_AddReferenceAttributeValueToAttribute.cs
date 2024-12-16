24fe31013cb9c4c41163d7b66830512c20d7bb99
thangnv-0703 <sunshines0703@gmail.com>
2024-12-16 23:35:17 +0700

    upload data model (#28)

---
+++ backend/src/NichoShop.Application/Migrations/20241216131312_AddReferenceAttributeValueToAttribute.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NichoShop.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReferenceAttributeValueToAttribute : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_product_attribute_values_AttributeId",
+                table: "product_attribute_values",
+                column: "AttributeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_product_attribute_values_attributes_AttributeId",
+                table: "product_attribute_values",
+                column: "AttributeId",
+                principalTable: "attributes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_product_attribute_values_attributes_AttributeId",
+                table: "product_attribute_values");
+
+            migrationBuilder.DropIndex(
+                name: "IX_product_attribute_values_AttributeId",
+                table: "product_attribute_values");
+        }
+    }
+}
