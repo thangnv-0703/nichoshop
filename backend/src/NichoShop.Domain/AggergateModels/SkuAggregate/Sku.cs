fa50872ed35cffcedce824b48726ca4ce3c51517
nguyenphuctinh <nguyenphuctinh>
2024-12-22 16:33:25 +0700

    change data type sku variants

---
+++ backend/src/NichoShop.Domain/AggergateModels/SkuAggregate/Sku.cs
@@ -0,0 +1,45 @@
+﻿using NichoShop.Domain.Enums;
+using NichoShop.Domain.SeedWork;
+using NichoShop.Domain.Shared;
+
+namespace NichoShop.Domain.AggergateModels.SkuAggregate;
+public class Sku : AggregateRoot<int>
+{
+    public string SkuNo { get; private set; }
+
+    public int ProductId { get; private set; }
+
+    private readonly List<SkuVariant> _skuVariants = [];
+
+    public IReadOnlyCollection<SkuVariant> SkuVariants => _skuVariants.AsReadOnly();
+
+    public int Quantity { get; private set; }
+
+    public bool InActive { get; private set; }
+
+    public Money Price { get; private set; }
+
+    private Sku()
+    {
+    }
+
+    public Sku(string skuNo, int productId, List<SkuVariant> skuVariants, int quantity, bool inActive, double amount, Currency currency)
+    {
+        SkuNo = skuNo;
+        ProductId = productId;
+        _skuVariants = skuVariants;
+        Quantity = quantity;
+        InActive = inActive;
+        Price = new Money(amount, currency);
+
+        if (IsInvalid())
+        {
+            throw new Exception("Invalid sku");
+        }
+    }
+
+    private bool IsInvalid()
+    {
+        return Quantity < 0;
+    }
+}
--- backend/src/NichoShop.Infrastructure/EntityConfigurations/SkuEntityConfiguration.cs
+++ backend/src/NichoShop.Infrastructure/EntityConfigurations/SkuEntityConfiguration.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using NichoShop.Domain.AggergateModels;
+using NichoShop.Domain.AggergateModels.SkuAggregate;
 using NichoShop.Domain.Enums;
 using NichoShop.Domain.Shared;
+using System.Text.Json;
 
 namespace NichoShop.Infrastructure.EntityConfigurations;
 public class SkuEntityConfiguration : IEntityTypeConfiguration<Sku>
@@ -23,9 +25,20 @@ public class SkuEntityConfiguration : IEntityTypeConfiguration<Sku>
         builder.Property(o => o.ProductId)
             .IsRequired();
 
-        builder.Property(o => o.ProducVarianttName)
-            .HasMaxLength(50)
-            .IsRequired(false);
+        builder.Property(pv => pv.SkuVariants)
+             .HasConversion(
+                 options => JsonSerializer.Serialize(options, (JsonSerializerOptions)null!),
+                 json => JsonSerializer.Deserialize<List<SkuVariant>>(json!, (JsonSerializerOptions)null)
+             )
+             .HasColumnType("jsonb") // Use PostgreSQL's JSONB column type
+             .HasColumnName("SkuVariants")
+             .IsRequired()
+             .Metadata.SetValueComparer(
+                 new ValueComparer<IReadOnlyCollection<SkuVariant>>(
+                 (c1, c2) => (c1 ?? new List<SkuVariant>()).SequenceEqual(c2 ?? new List<SkuVariant>()),
+                 c => c == null ? 0 : c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                 c => c == null ? new List<SkuVariant>() : c.ToList())
+             );
 
         builder.OwnsOne(o => o.Price, priceBuilder =>
         {
