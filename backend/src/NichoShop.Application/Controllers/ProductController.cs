075e703194e6d8e13bcf92def82850b04eaebee5
nguyenphuctinh <nguyenphuctinh>
2024-12-22 17:50:58 +0700

    add product controller

---
+++ backend/src/NichoShop.Application/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NichoShop.Application.Interfaces;
+using System.Net;
+
+namespace NichoShop.Application.Controllers
+{
+    [Route("api/v1/product")]
+    [ApiController]
+    //[Authorize]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPut("{productId}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetProductDetail(int productId)
+        {
+            var result = await _productService.GetProductDetailAsync(productId);
+            return Ok(result);
+        }
+    }
+}
