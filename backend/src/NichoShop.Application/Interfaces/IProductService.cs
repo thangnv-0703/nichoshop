ad076eedf95eb1a0cdf25e32de23f7c1d64cf022
littlelovevu <69585816+littlelovevu@users.noreply.github.com>
2024-12-27 20:49:54 +0700

    feature api search product (#41)

--- backend/src/NichoShop.Application/Interfaces/IProductService.cs
+++ backend/src/NichoShop.Application/Interfaces/IProductService.cs
@@ -1,9 +1,12 @@
-﻿using NichoShop.Domain.AggergateModels.ProductAggregate;
+﻿using NichoShop.Application.Models.Dtos.Request.Product;
+using NichoShop.Application.Models.ViewModels;
+using NichoShop.Domain.AggergateModels.ProductAggregate;
 
 namespace NichoShop.Application.Interfaces
 {
     public interface IProductService
     {
         Task<Product> GetProductDetailAsync(int productId);
+        Task<List<ProductSearchViewModel>> GetProductSearchViewModelAsync(ProductSearchRequestDto param);
     }
 }
--- backend/src/NichoShop.Application/Services/ProductService.cs
+++ backend/src/NichoShop.Application/Services/ProductService.cs
@@ -1,4 +1,7 @@
 ﻿using NichoShop.Application.Interfaces;
+using NichoShop.Application.Models.Dtos.Request.Product;
+using NichoShop.Application.Models.ViewModels;
+using NichoShop.Application.Queries;
 using NichoShop.Domain.AggergateModels.ProductAggregate;
 using NichoShop.Domain.Repositories;
 
@@ -6,16 +9,24 @@ namespace NichoShop.Application.Services
 {
     public class ProductService : IProductService
     {
+        private readonly IQueryService _queryService;
         private readonly IProductRepository _productRepository;
-        public ProductService(IProductRepository productRepository)
+
+        public ProductService(IProductRepository productRepository, IQueryService queryService)
         {
             _productRepository = productRepository;
+            _queryService = queryService;
         }
 
         public async Task<Product> GetProductDetailAsync(int productId)
         {
             var product = await _productRepository.GetByIdAsync(productId, includeDetail: true) ?? throw new Exception("Product not found");
             return product;
+        }
+
+        public async Task<List<ProductSearchViewModel>> GetProductSearchViewModelAsync(ProductSearchRequestDto param)
+        {
+            return await _queryService.GetProductSearchViewModelAsync(param);
         }
     }
 }
