ec44008a43f90cb74017ea4aa21cb23788b212ce
thangnv-0703 <sunshines0703@gmail.com>
2025-02-01 14:47:45 +0700

    update to use azure

--- backend/src/NichoShop.Application/Program.cs
+++ backend/src/NichoShop.Application/Program.cs
@@ -5,7 +5,24 @@ using NLog.Web;
 
 var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 var builder = WebApplication.CreateBuilder(args);
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+if (environment == "Development")
+{
+    builder.Configuration.AddAzureAppConfiguration(options =>
+    {
+        options.Connect("Endpoint=https://nichoshop-app-configuraion.azconfig.io;Id=OI5h;Secret=50sUfcbZ6LAPoXV92kOhnYTu0BB8W7KGcVGfSPeSE9v6eWvgdVQJJQQJ99BAAC3pKaRJiu6FAAACAZAC18S3");
+    });
+
+    //builder.Configuration.AddAzureKeyVault(
+    //    new Uri("<Key Vault URI>"),
+    //    new DefaultAzureCredential());
+}
+
+
 var configuration = builder.Configuration;
+Console.WriteLine(configuration["Test:Env"]);
 
 // Add services to the container.
 builder.Services
