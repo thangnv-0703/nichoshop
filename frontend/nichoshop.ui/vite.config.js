ffb23aec3f979301b4d324f9997e739beb44f81b
thangnv-0703 <sunshines0703@gmail.com>
2025-02-09 16:55:28 +0700

    Refactor (#82)
    * update code
    
    * update chunk size

--- frontend/nichoshop.ui/vite.config.js
+++ frontend/nichoshop.ui/vite.config.js
@@ -6,6 +6,9 @@ import { PrimeVueResolver } from "@primevue/auto-import-resolver";
 
 export default defineConfig({
   base: "/",
+  build: {
+    chunkSizeWarningLimit: 1024, // Set to 1024 KB (1MB) or higher to suppress warnings
+  },
   plugins: [
     vue(),
     Components({
