39f30c7d68285bda9cd13ccdf8e4ff79e7cddacd
thangnv-0703 <sunshines0703@gmail.com>
2025-02-09 15:45:11 +0700

    update vite config

--- frontend/nichoshop.ui/vite.config.js
+++ frontend/nichoshop.ui/vite.config.js
@@ -5,6 +5,7 @@ import Components from "unplugin-vue-components/vite";
 import { PrimeVueResolver } from "@primevue/auto-import-resolver";
 
 export default defineConfig({
+  base: "/",
   plugins: [
     vue(),
     Components({
