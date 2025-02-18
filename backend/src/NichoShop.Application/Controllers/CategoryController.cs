cbece27a174c793bde12a372141487d846849ec4
littlelovevu <69585816+littlelovevu@users.noreply.github.com>
2025-02-18 20:50:51 +0700

    sử dụng cache cho categories (#85)
    * product home
    
    * popup change address
    
    * feature sprint 2/2025
    
    * sử dụng cache cho categories

--- backend/src/NichoShop.Application/Controllers/CategoryController.cs
+++ backend/src/NichoShop.Application/Controllers/CategoryController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NichoShop.Application.Helpers;
 using NichoShop.Application.Interfaces;
+using NichoShop.Application.Models.ViewModels;
 using System.Net;
 
 namespace NichoShop.Application.Controllers;
@@ -10,17 +12,19 @@ namespace NichoShop.Application.Controllers;
 public class CategoryController : Controller
 {
     private readonly ICategoryService _categoryService;
+    private readonly ICacheService _redisService;
 
-    public CategoryController(ICategoryService categoryService)
+    public CategoryController(ICategoryService categoryService, ICacheService redisService)
     {
         _categoryService = categoryService;
+        _redisService = redisService;
     }
 
     [HttpGet]
     [ProducesResponseType((int)HttpStatusCode.OK)]
     public async Task<IActionResult> GetCategory()
     {
-        var result = await _categoryService.GetCategoryAsync();
+        var result = await _redisService.GetOrCreateAsync(CacheKeys.Categories, _categoryService.GetCategoryAsync);
         return Ok(result);
     }
 }
---
+++ backend/src/NichoShop.Application/Helpers/CacheKeys.cs
@@ -0,0 +1,6 @@
+﻿namespace NichoShop.Application.Helpers;
+
+public static class CacheKeys
+{
+    public const string Categories = "categories";
+}
