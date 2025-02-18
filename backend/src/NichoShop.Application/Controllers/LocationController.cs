be57df46db9a7990b1f83e6aeaa757d96f6277d9
Danh Vu <ledanhvu090888@gmail.com>
2025-02-18 21:32:04 +0700

    thi công cache

--- backend/src/NichoShop.Application/Controllers/LocationController.cs
+++ backend/src/NichoShop.Application/Controllers/LocationController.cs
@@ -11,17 +11,25 @@ namespace NichoShop.Application.Controllers;
 public class LocationController : Controller
 {
     private readonly ILocationService _locationService;
+    private readonly ICacheService _redisService;
 
-    public LocationController(ILocationService locationService)
+    public LocationController(ILocationService locationService, ICacheService redisService)
     {
         _locationService = locationService;
+        _redisService = redisService;
     }
 
     [HttpGet]
     [ProducesResponseType((int)HttpStatusCode.OK)]
     public async Task<IActionResult> GetLocation(int type, string? parentCode)
     {
-        var result = await _locationService.GetLocationAsync(type, parentCode);
+        var cacheKey = $"location_{type}_{parentCode}";
+
+        var result = await _redisService.GetOrCreateAsync(cacheKey, async () =>
+        {
+            return await _locationService.GetLocationAsync(type, parentCode);
+        });
+
         return Ok(result);
     }
 }
