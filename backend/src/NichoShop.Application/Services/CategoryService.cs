0e96a559805f36d3db81f326818fa983bc602ff2
Danh Vu <ledanhvu090888@gmail.com>
2025-02-20 22:54:52 +0700

    refactor use redis

--- backend/src/NichoShop.Application/Services/CategoryService.cs
+++ backend/src/NichoShop.Application/Services/CategoryService.cs
@@ -1,15 +1,18 @@
-﻿using NichoShop.Application.Interfaces;
+﻿using NichoShop.Application.Helpers;
+using NichoShop.Application.Interfaces;
 using NichoShop.Application.Models.ViewModels;
 using NichoShop.Application.Queries;
 
 namespace NichoShop.Application.Services;
 
-public class CategoryService(IQueryService queryService) : ICategoryService
+public class CategoryService(IQueryService queryService, ICacheService redisService) : ICategoryService
 {
     private readonly IQueryService _queryService = queryService;
+    private readonly ICacheService _redisService = redisService;
+
 
     public async Task<List<CategoryViewModel>> GetCategoryAsync()
     {
-        return await _queryService.GetCategoryViewModelsAsync();
+        return await _redisService.GetOrCreateAsync(CacheKeys.Categories, _queryService.GetCategoryViewModelsAsync);
     }
 }
--- backend/src/NichoShop.Application/Services/LocationService.cs
+++ backend/src/NichoShop.Application/Services/LocationService.cs
@@ -4,12 +4,20 @@ using NichoShop.Application.Queries;
 
 namespace NichoShop.Application.Services;
 
-public class LocationService(IQueryService queryService) : ILocationService
+public class LocationService(IQueryService queryService, ICacheService redisService) : ILocationService
 {
     private readonly IQueryService _queryService = queryService;
+    private readonly ICacheService _redisService = redisService;
 
-    public async Task<List<LocationViewModel>> GetLocationAsync(int type, string parentCode)
+    public async Task<List<LocationViewModel>> GetLocationAsync(int type, string? parentCode)
     {
-        return await _queryService.GetLocationViewModelsAsync(type, parentCode);
+        var cacheKey = $"location_{type}_{parentCode}";
+
+        var result = await _redisService.GetOrCreateAsync(cacheKey, async () =>
+        {
+            return await _queryService.GetLocationViewModelsAsync(type, parentCode);
+        });
+
+        return result;
     }
 }
